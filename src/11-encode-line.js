f5a770277478759c84048ad8fc2bc51b2b391b57
Hanna Shatsko <NytaShetko@yandex.ru>
2021-04-11 13:47:55 +0300

    feat: add 11 solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,24 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (str.length === 0) {
+    return str;
+  }
+
+  let result = '';
+  let counter = 1;
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === str[i]) {
+      counter++;
+    } else {
+      result += `${counter === 1 ? '' : counter}${str[i - 1]}`;
+      counter = 1;
+    }
+  }
+  result += `${counter === 1 ? '' : counter}${str[str.length - 1]}`;
+  return result;
 }
 
 module.exports = encodeLine;
